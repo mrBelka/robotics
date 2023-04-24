6c8dd32384976c6dbe441a0a56ae4b488ebf28ff
mishklgpmi <mishklgpmi@mail.ru>
2023-04-24 23:03:34 +0200

    work navigation

---
+++ telerobot_gazebo/launch/gazebo.launch.py
@@ -0,0 +1,59 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    use_sim_time = True
+
+    ekf_config_path = PathJoinSubstitution(
+        [FindPackageShare("telerobot_base"), "config", "ekf.yaml"]
+    )
+
+    world_path = PathJoinSubstitution(
+        [FindPackageShare("telerobot_gazebo"), "worlds", "test.world"]
+    )
+
+    description_launch_path = PathJoinSubstitution(
+        [FindPackageShare('telerobot_description'), 'launch', 'description.launch.py']
+    )
+
+    return LaunchDescription([
+        ExecuteProcess(
+            cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so',  '-s', 'libgazebo_ros_init.so', world_path],
+            output='screen'
+        ),
+
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            name='urdf_spawner',
+            output='screen',
+            arguments=["-topic", "robot_description", "-entity", "telerobot"]
+        ),
+
+        Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_filter_node',
+            output='screen',
+            parameters=[
+                {'use_sim_time': use_sim_time}, 
+                ekf_config_path
+            ],
+            remappings=[("odometry/filtered", "odom")]
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(description_launch_path),
+            launch_arguments={
+                'use_sim_time': str(use_sim_time),
+                'publish_joints': 'false',
+            }.items()
+        ),
+
+    ])
