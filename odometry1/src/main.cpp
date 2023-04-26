0ccb5eba6d3695f7174fd27fe929fdc40b8870cd
mrBelka <mishklgpmi@mail.ru>
2023-04-26 20:23:08 +0200

    bag file

---
+++ odometry1/src/main.cpp
@@ -0,0 +1,205 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+
+#include "retrans.h"
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "telerobot_interfaces/msg/motor.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "telerobot_interfaces/msg/motor.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2/LinearMath/Quaternion.h>
+
+
+#define TICK_TO_RAD 0.00253354246
+
+using std::placeholders::_1;
+
+class Odometry : public rclcpp::Node
+{
+  public:
+    Odometry() : Node("telebot_odometry")
+    {
+        m_encodes_sub = this->create_subscription<telerobot_interfaces::msg::Motor>(
+            "encodes", 10, std::bind(&Odometry::encodes_callback, this, _1));
+
+        m_joint_states_pub = this->create_publisher<sensor_msgs::msg::JointState>("joint_states", 10);
+
+        m_last_time = this->now();
+
+        odom_class = std::make_unique<OdometryClass>(0.04, 0.125, 0.11);
+        m_pub = this->create_publisher<telerobot_interfaces::msg::Motor>("wheel_commands", 10);
+        m_odom_commands_sub = this->create_subscription<geometry_msgs::msg::Twist>(
+                "cmd_vel", 10, std::bind(&Odometry::odom_commands, this, _1));
+        m_odom_pub = this->create_publisher<nav_msgs::msg::Odometry>("odom", 10);
+        m_tf_broadcaster = std::make_unique<tf2_ros::TransformBroadcaster>(this);
+    }
+
+  private:
+
+    void encodes_callback(const telerobot_interfaces::msg::Motor& encodes_msg)
+    {
+        rclcpp::Time now = this->now();
+        double motor_lf_position = TICK_TO_RAD * encodes_msg.motor_lf;
+        double motor_rf_position = TICK_TO_RAD * encodes_msg.motor_rf;
+        double motor_lr_position = TICK_TO_RAD * encodes_msg.motor_lr;
+        double motor_rr_position = TICK_TO_RAD * encodes_msg.motor_rr;
+
+        // Joint states block
+        auto joint_states_msg = std::make_unique<sensor_msgs::msg::JointState>();
+        joint_states_msg->header.frame_id = "base_link";
+        joint_states_msg->header.stamp = now;
+        joint_states_msg->name.emplace_back("front_left_wheel_joint");
+        joint_states_msg->name.emplace_back("front_right_wheel_joint");
+        joint_states_msg->name.emplace_back("rear_left_wheel_joint");
+        joint_states_msg->name.emplace_back("rear_right_wheel_joint");
+
+        joint_states_msg->position.push_back(motor_lf_position);
+        joint_states_msg->position.push_back(motor_rf_position);
+        joint_states_msg->position.push_back(motor_lr_position);
+        joint_states_msg->position.push_back(motor_rr_position);
+
+        m_joint_states_pub->publish(std::move(joint_states_msg));
+
+        // Odometry block
+        if(m_first)
+        {
+            m_last_joint_positions[0] = motor_lf_position;
+            m_last_joint_positions[1] = motor_rf_position;
+            m_last_joint_positions[2] = motor_lr_position;
+            m_last_joint_positions[3] = motor_rr_position;
+            m_first = false;
+            return;
+        }
+        m_diff_joint_positions[0] = motor_lf_position - m_last_joint_positions[0];
+        m_diff_joint_positions[1] = motor_rf_position - m_last_joint_positions[1];
+        m_diff_joint_positions[2] = motor_lr_position - m_last_joint_positions[2];
+        m_diff_joint_positions[3] = motor_rr_position - m_last_joint_positions[3];
+        m_last_joint_positions[0] = motor_lf_position;
+        m_last_joint_positions[1] = motor_rf_position;
+        m_last_joint_positions[2] = motor_lr_position;
+        m_last_joint_positions[3] = motor_rr_position;
+
+
+
+        rclcpp::Duration duration(rclcpp::Duration::from_nanoseconds(
+                now.nanoseconds() - m_last_time.nanoseconds()));
+        double step_time = duration.seconds();
+
+        double wheel_lf = m_diff_joint_positions[0];
+        double wheel_rf = m_diff_joint_positions[1];
+        double wheel_lr = m_diff_joint_positions[2];
+        double wheel_rr = m_diff_joint_positions[3];
+
+        FORWARD_DATA forw_data {wheel_lf, wheel_rf, wheel_lr, wheel_rr};
+
+        odom_class->Update(step_time, forw_data);
+
+        RCLCPP_INFO(this->get_logger(), "%f", odom_class->posx);
+        RCLCPP_INFO(this->get_logger(), "%f", odom_class->posy);
+        RCLCPP_INFO(this->get_logger(), "%f", odom_class->rot);
+
+        m_robot_pos[0] = odom_class->posx;
+        m_robot_pos[1] = odom_class->posy;
+        m_robot_pos[2] = odom_class->rot;
+
+        m_robot_vel[0] = odom_class->mv_after_update.VX;
+        m_robot_vel[1] = odom_class->mv_after_update.VY;
+        m_robot_vel[2] = odom_class->mv_after_update.WZ;
+
+        // Odom publishing
+        auto odom_msg = std::make_unique<nav_msgs::msg::Odometry>();
+
+        odom_msg->header.frame_id = "odom";
+        odom_msg->child_frame_id = "base_footprint";
+        odom_msg->header.stamp = now;
+
+        odom_msg->pose.pose.position.x = m_robot_pos[0];
+        odom_msg->pose.pose.position.y = m_robot_pos[1];
+        odom_msg->pose.pose.position.z = 0;
+
+        tf2::Quaternion q;
+        q.setRPY(0.0, 0.0, m_robot_pos[2]);
+
+        odom_msg->pose.pose.orientation.x = q.x();
+        odom_msg->pose.pose.orientation.y = q.y();
+        odom_msg->pose.pose.orientation.z = q.z();
+        odom_msg->pose.pose.orientation.w = q.w();
+
+        odom_msg->twist.twist.linear.x = m_robot_vel[0];
+        odom_msg->twist.twist.linear.y = m_robot_vel[1];
+        odom_msg->twist.twist.angular.z = m_robot_vel[2];
+
+        geometry_msgs::msg::TransformStamped odom_tf;
+
+        odom_tf.transform.translation.x = odom_msg->pose.pose.position.x;
+        odom_tf.transform.translation.y = odom_msg->pose.pose.position.y;
+        odom_tf.transform.translation.z = odom_msg->pose.pose.position.z;
+        odom_tf.transform.rotation = odom_msg->pose.pose.orientation;
+
+        odom_tf.header.frame_id = "odom";
+        odom_tf.child_frame_id = "base_footprint";
+        odom_tf.header.stamp = now;
+
+        m_odom_pub->publish(std::move(odom_msg));
+
+        m_tf_broadcaster->sendTransform(odom_tf);
+
+        m_last_time = this->now();
+
+    }
+
+
+    void odom_commands(const geometry_msgs::msg::Twist & msg)
+    {
+        
+        RCLCPP_INFO(this->get_logger(), "%f", msg.linear.x);
+        RCLCPP_INFO(this->get_logger(), "%f", msg.linear.y);
+        RCLCPP_INFO(this->get_logger(), "%f", msg.angular.z);
+
+        auto msg1 = telerobot_interfaces::msg::Motor();
+
+        {
+            INVERSE_DATA inv_data{msg.linear.x, msg.linear.y, msg.angular.z};
+            FORWARD_DATA data = odom_class->GetOdometryINV(inv_data);
+            //std::lock_guard<std::mutex> lock(m_mutex);
+
+            msg1.motor_lf = data.WFL * 100;
+            msg1.motor_rf = data.WFR * 100;
+            msg1.motor_lr = data.WRL * 100;
+            msg1.motor_rr = data.WRR * 100;
+        }
+
+        m_pub->publish(msg1);
+
+    }
+
+    rclcpp::Subscription<telerobot_interfaces::msg::Motor>::SharedPtr m_encodes_sub;
+    rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr m_joint_states_pub;
+
+    double m_diff_joint_positions[4] = {0};
+    double m_last_joint_positions[4] = {0};
+    bool m_first = true;
+
+    double m_robot_pos[3] = {0,0,0};
+    double m_robot_vel[3] = {0,0,0};
+
+    rclcpp::Time m_last_time;
+    std::unique_ptr<OdometryClass> odom_class;
+    rclcpp::Publisher<telerobot_interfaces::msg::Motor>::SharedPtr m_pub;
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr m_odom_commands_sub;
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr m_odom_pub;
+    std::unique_ptr<tf2_ros::TransformBroadcaster> m_tf_broadcaster;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<Odometry>());
+  rclcpp::shutdown();
+  return 0;
+}
---
+++ odometry1/src/retrans.h
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <cmath>
+
+#define PI 3.141592653589793238
+
+
+struct FORWARD_DATA{
+		double WFL;
+		double WFR;
+		double WRL;
+		double WRR;
+		FORWARD_DATA() = default;
+};
+
+
+struct INVERSE_DATA {
+	double VX;
+	double VY;
+	double WZ;
+
+	INVERSE_DATA() = default;
+};
+
+
+struct OdometryClass {
+	double r, lx, ly;
+	double trans_matrix[3][3];
+	double vector[2];
+	double posx = 0.0, posy = 0.0, rot = 0;
+	INVERSE_DATA mv_after_update;
+	OdometryClass(
+		double r,
+		double lx,
+		double ly
+		): r(r), lx(lx), ly(ly) {}
+
+	void Update(double dt, FORWARD_DATA data) {
+		std::cout << data.WFL << " " << data.WFR << " " << data.WRL << " " << data.WRR << std::endl;
+		mv_after_update = GetOdometryFORW(data);
+		INVERSE_DATA& mv = mv_after_update;
+
+		std::cout << mv.VX << " " << mv.VY << " " << mv.WZ << std::endl;
+
+		vector[0] = mv.VX;
+		vector[1] = mv.VY;
+
+		rot += mv.WZ;
+		posx = (std::cos(rot)*vector[0] - std::sin(rot)*vector[1] + posx);
+		posy = (std::sin(rot)*vector[0] + std::cos(rot)*vector[1] + posy);
+	}
+
+	void Update(double dt, INVERSE_DATA mv) {
+		vector[0] = mv.VX;
+		vector[1] = mv.VY;
+
+		posx = (std::cos(mv.WZ)*vector[0]*dt - std::sin(mv.WZ)*vector[1]*dt + posx);
+		posy = (std::sin(mv.WZ)*vector[0]*dt + std::cos(mv.WZ)*vector[1]*dt + posy);
+	}
+
+
+	INVERSE_DATA GetOdometryFORW(FORWARD_DATA data) {
+		return {
+			(data.WFL + data.WFR + data.WRL + data.WRR) * (r/4),
+			(-data.WFL + data.WFR + data.WRL - data.WRR) * (r/4),
+			(-data.WFL + data.WFR - data.WRL + data.WRR) * (r/4) * (1/(lx + ly))
+		};
+	}
+
+	FORWARD_DATA GetOdometryINV(INVERSE_DATA data) {
+		return {
+			(1/r) * (data.VX - data.VY - (lx + ly)*data.WZ),
+			(1/r) * (data.VX + data.VY + (lx + ly)*data.WZ),
+			(1/r) * (data.VX + data.VY - (lx + ly)*data.WZ),
+			(1/r) * (data.VX - data.VY + (lx + ly)*data.WZ)
+		};
+	}
+};
+
